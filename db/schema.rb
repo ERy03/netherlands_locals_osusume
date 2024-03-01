c0265d7c9658d359c4b0608294396fe6e6007a05
Ryota <ryoenoki2013@gmail.com>
2024-03-01 23:27:30 +0100

    add: recommendation model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_184133) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_01_212925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recommendations", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.text "description"
+    t.date "visit_date"
+    t.string "address"
+    t.integer "recommendation_type"
+    t.float "price"
+    t.float "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_01_184133) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recommendations", "users"
 end
