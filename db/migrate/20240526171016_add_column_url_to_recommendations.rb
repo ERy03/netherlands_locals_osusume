a06ec6e55e973a192efca593d49edc4ef7d6566a
Ryota <ryoenoki2013@gmail.com>
2024-05-26 19:38:31 +0200

    update: add image url to recommendations

---
+++ db/migrate/20240526171016_add_column_url_to_recommendations.rb
@@ -0,0 +1,5 @@
+class AddColumnUrlToRecommendations < ActiveRecord::Migration[7.1]
+  def change
+    add_column :recommendations, :image_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_26_144828) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_171016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_26_144828) do
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["user_id"], name: "index_recommendations_on_user_id"
   end
 
