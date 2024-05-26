41ee5c6c09c30e1a5da7386748fc0e0171b65e93
Ryota <ryoenoki2013@gmail.com>
2024-05-26 19:38:55 +0200

    update: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,41 @@
+require 'open-uri'
+require 'faker'
+
 puts "Cleaning database..."
-User.destroy_all
 Recommendation.destroy_all
+User.destroy_all
 
 puts "Creating user..."
 
-user = User.create!(
+User.create!(
   email: "test@test.com",
   user_name: "Bob",
   password: '123456',
 )
 
-Recommendation.create!(
-  name: "Winkel 43",
-  user_id: user.id,
-  description: "The Noordermarkt, in the heart of the Jewish quarter the 'Jordaan', is known for its friendly and relaxed atmosphere. Right on the corner – where the Westerstraat runs into the the Noordermarkt - Winkel43 has been established in an attractive house, which was built in traditional Amsterdam. fashion. Because of its privileged location you will be able to enjoy a cup of coffee or lunch on our terrace on sunny days all year round. The restaurant opens early in the morning till ten in the evening, but the bar won’t close until midnight.",
-  address: "some where",
-  recommendation_type: 1,
-  price: 10,
-  rating: 4.2
-)
+# Seed data
+10.times do |i|
+  # Generate random name and description using Faker
+  name = Faker::Restaurant.name
+  description = Faker::Restaurant.description
 
-puts "Finished"
+  # Fetch a random image URL from Unsplash
+  image_url = "https://source.unsplash.com/random/?restaurant"
+
+  # Create a new recommendation record
+  Recommendation.create!(
+    name: name,
+    user_id: User.first.id,
+    description: description,
+    visit_date: Date.today,
+    address: Faker::Address.full_address,
+    recommendation_type: rand(0..8), # Assuming you have 9 types from 0 to 8
+    price: rand(10.0..100.0).round(2),
+    rating: rand(1.0..5.0).round(1),
+    image_url: image_url
+  )
+
+  puts "Created recommendation ##{i+1} with image URL"
+end
+
+puts "Seeding completed!"
