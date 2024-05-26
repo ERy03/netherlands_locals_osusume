ca6043b08c20c761bcb7f499da305692ac8b700f
Ryota <ryoenoki2013@gmail.com>
2024-05-26 17:08:40 +0200

    update: set dete.today if no visit_date is provided

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,8 +1,28 @@
 class Recommendation < ApplicationRecord
   belongs_to :user
 
-  # TODO: add more validations if necessary
+  before_validation :set_default_visit_date, on: :create
+
+  # Validations
   validates :name, presence: true, uniqueness: true
-  validates :visit_date, comparison: {less_than_or_equal_to: Date.today}
-  enum recommendation_type: { 'Other': 8, 'Scenery': 7, 'Market': 6, 'Bakery': 5, 'Park': 4, 'Event': 3, 'Shop': 2, 'Cafe': 1, 'Restaurant': 0 }
+  validates :visit_date, comparison: { less_than_or_equal_to: Date.today }
+
+  # Enum for recommendation_type
+  enum recommendation_type: {
+    'Other': 8,
+    'Scenery': 7,
+    'Market': 6,
+    'Bakery': 5,
+    'Park': 4,
+    'Event': 3,
+    'Shop': 2,
+    'Cafe': 1,
+    'Restaurant': 0
+  }
+
+  private
+
+  def set_default_visit_date
+    self.visit_date ||= Date.today
+  end
 end
