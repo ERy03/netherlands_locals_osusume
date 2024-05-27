02ead8dc78476491d7868701202b108c4a8e55d5
Ryota <ryoenoki2013@gmail.com>
2024-05-27 03:34:34 +0200

    add: stimulus hover controller

---
+++ app/javascript/controllers/heart_icon_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="heart-icon"
+export default class extends Controller {
+  static targets = ["heartIcon"];
+
+  hover() {
+    this.heartIconTarget.classList.remove("fa-regular");
+    this.heartIconTarget.classList.add("fa-solid");
+    this.heartIconTarget.style.color = "rgba(255, 100, 100, 0.869)";
+  }
+
+  unhover() {
+    this.heartIconTarget.classList.remove("fa-solid");
+    this.heartIconTarget.classList.add("fa-regular");
+    this.heartIconTarget.style.color = "";
+  }
+}
