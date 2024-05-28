0cce7e78f05b53e4b37b4458cc6ffe6b0afae993
Ryota <ryoenoki2013@gmail.com>
2024-05-28 18:01:29 +0200

    update: controller with filter and sort

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -7,8 +7,17 @@ class RecommendationsController < ApplicationController
     @recommendation_count = Recommendation.count
 
     if params[:query].present?
-        @recommendations = @recommendations.search_by_name(params[:query])
+        @recommendations = @recommendations.where("name ILIKE ?", "%#{params[:query]}%")
         @recommendation_count = @recommendations.length()
+    end
+
+    if params[:sort].present?
+      @recommendations = sort_recommendations(@recommendations, params[:sort])
+    end
+
+    if params[:filter].present?
+      @recommendations = filter_recommendations(@recommendations, params[:filter])
+      @recommendation_count = @recommendations.length()
     end
 
     respond_to do |format|
@@ -25,4 +34,49 @@ class RecommendationsController < ApplicationController
   def set_recommendation
     @recommendation = Recommendation.find(params[:id])
   end
+
+  def sort_recommendations(recommendations, sort)
+    case sort
+    when "top_rated"
+      recommendations.order(rating: :desc)
+    when "low_rated"
+      recommendations.order(rating: :asc)
+    when "newly_added"
+      recommendations.order(created_at: :desc)
+    when "old"
+      recommendations.order(created_at: :asc)
+    when "az"
+      recommendations.order(name: :asc)
+    when "za"
+      recommendations.order(name: :desc)
+    else
+      recommendations
+    end
+  end
+
+  def filter_recommendations(recommendations, filter)
+    case filter
+    when 'Restaurant'
+      recommendations.where(recommendation_type: :Restaurant)
+    when 'Cafe'
+      recommendations.where(recommendation_type: :Cafe)
+    when 'Shop'
+      recommendations.where(recommendation_type: :Shop)
+    when 'Event'
+      recommendations.where(recommendation_type: :Event)
+    when 'Park'
+      recommendations.where(recommendation_type: :Park)
+    when 'Bakery'
+      recommendations.where(recommendation_type: :Bakery)
+    when 'Market'
+      recommendations.where(recommendation_type: :Market)
+    when 'Scenery'
+      recommendations.where(recommendation_type: :Scenery)
+    when 'Other'
+      recommendations.where(recommendation_type: :Other)
+    else
+      recommendations
+    end
+  end
+  
 end
