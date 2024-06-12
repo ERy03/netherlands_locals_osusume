9f8d523a9b4ce91b97b0a943870cb932573304c4
Ryota <ryoenoki2013@gmail.com>
2024-06-13 01:25:22 +0200

    update: star rating

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -3,6 +3,6 @@ import StarRating from "star-rating.js";
 
 export default class extends Controller {
   connect() {
-    new StarRating(this.element, { tooltip: false });
+    new StarRating(this.element, { tooltip: false, clearable: false });
   }
 }
