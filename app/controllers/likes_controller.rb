8af77f07b6c791b7866bbc4a8b0bb9d56a2070c6
Ryota <ryoenoki2013@gmail.com>
2024-07-05 23:53:32 +0900

    update: likes controller with create and destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  before_action :find_recommendation
+
+  def create
+    @recommendation.likes.create(user: current_user)
+    redirect_to @recommendation
+  end
+
+  def destroy
+    @recommendation.likes.find_by(user: current_user).destroy
+    redirect_to @recommendation
+  end
+
+  private
+
+  def find_recommendation
+    @recommendation = Recommendation.find(params[:recommendation_id])
+  end
 end
