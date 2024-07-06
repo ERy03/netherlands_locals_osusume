6aeaa5d822811d88bbdcb67ba50206ed8a6e540f
Ryota <ryoenoki2013@gmail.com>
2024-07-06 20:00:48 +0900

    update: likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,29 @@ class LikesController < ApplicationController
   before_action :find_recommendation
 
   def create
-    @recommendation.likes.create(user: current_user)
-    redirect_to @recommendation
+    if current_user
+      like = @recommendation.likes.create(user: current_user)
+      if like.save
+        render json: { liked: true, likes_count: @recommendation.likes.count }, status: :created
+      else
+        render json: { errors: like.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { status: 'error', message: 'User must be logged in to like a recommendation' }
+    end
   end
 
   def destroy
-    @recommendation.likes.find_by(user: current_user).destroy
-    redirect_to @recommendation
+    if current_user
+      like = @recommendation.likes.find_by(user: current_user)
+      if like&.destroy
+        render json: { liked: false, likes_count: @recommendation.likes.count }, status: :ok
+      else
+        render json: { errors: ["Unable to unlike recommendation"] }, status: :unprocessable_entity
+      end
+    else
+      render json: { status: 'error', message: 'User must be logged in to unlike a recommendation' }
+    end
   end
 
   private
